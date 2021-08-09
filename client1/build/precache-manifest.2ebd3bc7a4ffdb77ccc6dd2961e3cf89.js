1ffc7606ee6907e8a637386a965a97b0a2bd0bb3
Jayanth <jayanth@Users-MacBook-Air.local>
2021-08-09 18:42:53 +0530

    l

---
+++ client1/build/precache-manifest.2ebd3bc7a4ffdb77ccc6dd2961e3cf89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e609d4f18460473c9ea224dbead6d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020d5ae267955c549f07",
+    "url": "/static/js/2.889f5cc1.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.889f5cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f2eb8182838208ee92",
+    "url": "/static/js/main.18172d4c.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
---
+++ client1/src/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+// axios makes api calls
+const API = axios.create({baseURL:'https://awesomexperience.herokuapp.com'})
+
+// API.interceptors.request.use((req)=>{
+
+// });
+//const url = 'https://awesomexperience.herokuapp.com/posts'
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+  
+export const fetchPosts = () => API.get('/posts');
+
+export const createPost = (newPost) => API.post('/posts',newPost);
+
+export const updatePost = (id,updatedPost) => API.patch(`/posts/${id}`,updatedPost);
+
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData)=> API.post('/user/signin',formData);
+
+export const signUp = (formData)=> API.post('/user/signup',formData);
